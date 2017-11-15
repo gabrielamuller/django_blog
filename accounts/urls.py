c422d771c9a722e6beb008a4b4383f2722baa117
Gabriela Muller <mullergabrielaa@gmail.com>
2017-11-15 12:56:29 +0000

    Initial commit

---
+++ accounts/urls.py
@@ -0,0 +1,7 @@
+from django.conf.urls import url
+from .views import logout, login, register
+urlpatterns = [
+    url(r'^logout', logout),
+    url(r'^login', login),
+    url(r'^register', register)
+    ]
